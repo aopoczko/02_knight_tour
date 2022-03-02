048afe27284997d8418255f6c4b46d4bd7ce352e
aopoczko <61934810+aopoczko@users.noreply.github.com>
2022-03-02 16:19:16 +0100

    6/6
    Had to rely on a solution found elsewhere for recursion/backtracking part:
    
    https://www.geeksforgeeks.org/the-knights-tour-problem-backtracking-1/
    
    Otherwise 10/10 would tour again

--- knights_tour.py
+++ knights_tour.py
@@ -1,23 +1,18 @@
 
-def draw_board(): #draw actual board state with formatting - not used in later stages
+def draw_board(board): #draw actual board state with formatting - not used in later stages
 
     dash_bar()
-
     for i in range(b):
-        #print("".join([str(b - i).rjust(cell - 1), "|"]))
-        print("".join([str(b - i).rjust(cell - 1), "|"]), " ".join([board[i][j].rjust(len(str(a * b)), "_") if board[i][j] != "X" else board[i][j].rjust(len(str(a * b)))  for j in range(a)]), "|")
-        #print(" ".join(board[i]).rjust(cell, "_"))
-        #print(i)
-        #print("".join([str(b - i).rjust(cell - 1), "|"]), " ".join([(board[i][j].rjust(cell, "_")) for j in range(a)]), "|")
+        print("".join([str(b - i).rjust(cell - 1), "|"]), " ".join(
+            [str(board[i][j]).rjust(len(str(a * b)), "_") if board[i][j] == "_" else str(board[i][j]).rjust(len(str(a * b)), " ")
+             for j in range(a)]), "|")
     dash_bar()
-    #print(" " * (cell - 1), "-" * ((len(str(a * b)) + 1) * a), "---", sep = "")
-    print(" " * (len(str(a)) +1), " ".join([(str(r+1).rjust(len(str(a * b)))) for r in range(a)]))
-    #print(" ".join(["_" * len(str(a*b)) for _ in range(b)]))
+    print(" " * (len(str(a)) + 1), " ".join([(str(r + 1).rjust(cell)) for r in range(a)]))
 
 def check_moves(): #marks available moves on the board
 
     count = 0
-    moves = [[2, 1], [2, -1], [-2, 1], [-2, -1], [1, 2], [1, -2], [-1, 2], [-1, -2]] #movement vectors from current position [x, y]
+    moves = [[2, 1], [2, -1], [-2, 1], [-2, -1], [1, 2], [1, -2], [-1, 2], [-1, -2]]  #movement vectors  [x, y]
     for i in range(8):
         #for j in range(2):
         if y + moves[i][0] > b or y + moves[i][0] < 0 or x + moves[i][1] > a or x + moves[i][1] < 0:
@@ -32,17 +27,12 @@ def check_moves(): #marks available moves on the board
             except:
                 continue
 
-    #print()
-    #print(" " * (cell - 1), "-" * ((cell + 1) * a), "---", sep = "")
-
     dash_bar()
     for i in range(b):
-        print("".join([str(b - i).rjust(cell - 1), "|"]), " ".join([board[i][j].rjust(len(str(a * b)), "_") if board[i][j] == "_" else board[i][j].rjust(len(str(a * b)), " ")  for j in range(a)]), "|")
+        print("".join([str(b - i).rjust(cell - 1), "|"]), " ".join([str(board[i][j]).rjust(len(str(a * b)), "_") if board[i][j] == "_" else str(board[i][j]).rjust(len(str(a * b)), " ")  for j in range(a)]), "|")
     dash_bar()
-    print(" " * (len(str(a)) + 1), " ".join([(str(r + 1).rjust(cell)) for r in range(a)]))
-    #print(" ".join(["_" * len(str(a*b)) for _ in range(b)]))
+    print(" " * (len(str(a))), " ".join([(str(r).rjust(cell)) for r in range(a)]))
 
-    #print(x, y)
     return count
 
 def count_moves(y, x): #subfunciton for check_moves(), counts available moves from tiles determined in master function
@@ -56,10 +46,7 @@ def count_moves(y, x): #subfunciton for check_moves(), counts available moves fr
         else:
             try:
                 if board[y + moves[i][0]][x + moves[i][1]] == "_":
-                    #print(x + moves[i][1], y + moves[i][0])
                     count += 1
-                    #print(f"ok  {moves[i]}")
-                    #print(f"ok {moves[i][0]}, {knight_x + moves[i][1]}")
             except:
                 continue
     return count
@@ -136,20 +123,51 @@ def ask_move(): #ask user about knight's next move
             continue
         else:
             x_move, y_move = int(x_move) - 1, b - int(y_move)
-            #print(f"x: {x} y: {y}")
-            #print(f"x: {x_move} y: {y_move}")
             for i in range(8):
                 if y + moves[i][0] == y_move and x + moves[i][1] == x_move and y + moves[i][0] <= b and y + moves[i][0] >= 0 and x + moves[i][1] <= a and x + moves[i][1] >= 0 and board[y_move][x_move] != "X":
-                    #print(f"x: {x} y: {y}")
-                    #print(f"x: {x_move} y: {y_move}")
                     board[y_move][x_move] = "X"
                     print()
                     board[y][x] = "*"
-
-
                     return x_move, y_move
 
         pos = input("Invalid move! Enter your next move: ")
+
+def solve():
+    auto_board = [[-1 for i in range(a)] for j in range(b)]
+
+    #first tile starts at #1
+    auto_board[y][x] = 1
+    moves = [[2, 1], [2, -1], [-2, 1], [-2, -1], [1, 2], [1, -2], [-1, 2], [-1, -2]]  #movement vectors table [y, x]
+
+    #next tile is #2
+    steps = 2
+
+    #checking if solution exists or not
+    if (not next_move(auto_board, x, y, moves, steps)):
+        print("No solution exists!")
+        return False
+    else:
+        if choice == "n":
+            print("Here's the solution!")
+            draw_board(auto_board)
+
+def next_move(board, x, y, moves, steps): #checking if valid route exists and passing it if it does
+    if (steps == int(a) * int(b) + 1):
+        return True
+
+
+    for i in range(8):
+        new_x = x + moves[i][1]
+        new_y = y + moves[i][0]
+        if y + moves[i][0] < b and y + moves[i][0] >= 0 and x + moves[i][1] < a and x + moves[i][1] >= 0 and board[new_y][new_x] == -1:
+            board[new_y][new_x] = steps
+
+            if (next_move(board, new_x, new_y, moves, steps + 1)):
+                return True
+
+            #backtracking
+            board[new_y][new_x] = -1
+    return False
 
 def board_clean():
     for i in range(b):
@@ -157,42 +175,56 @@ def board_clean():
             if board[i][j] in ["0", "1", "2", "3", "4", "5", "6", "7", "8"]:
                 board[i][j] = "_"
 
-
-
+def start():
+    answer = input("Do you want to try the puzzle? (y/n): ")
+    while answer != "y" and answer != "n":
+        if answer != "y" and answer != "n":
+            print(invalid_input)
+        answer = input("Do you want to try the puzzle? (y/n): ")
+    return answer
 
 #starting setup
 
 #errors
 invalid_dim = "Invalid dimensions!"
 invalid_pos = "Invalid position!"
+no_solution = "No solution exists!"
+invalid_input = "Invalid input!"
 
-#getting board dimensions and knight's starting position, marking knight with X
+#getting board dimensions and knight's starting position
+
 a, b = ask_board()
-global x
-global y
+
 x, y = ask_pos()
 cell = len(str(b)) + 1  # calculates cell width for drawing board
-board = [["_" for i in range(a)] for j in range(b)]
-
-#default board state
-board[y][x] = "X"
 
 squares = 1
 available_moves = 0
 
-while True:
-    board_clean()
-    available_moves = check_moves()
-    print(f"squares: {squares}")
-    if available_moves == 0:
-        if squares == a * b:
-            print("What a great tour! Congratulations!")
-            break
-        else:
-            print("No more possible moves!")
-            print(f"Your knight visited {squares} squares!")
-            break
-    else:
-        squares += 1
-    x, y = ask_move()
+choice = start()
 
+board = [["_" for _ in range(a)] for _ in range(b)]
+
+if choice == "y": #manual gameplay
+    board[y][x] = "X"
+
+    if solve() == False:
+        pass
+    else:
+        while True:
+            board_clean()
+            available_moves = check_moves()
+            print(f"squares: {squares}")
+            if available_moves == 0:
+                if squares == a * b:
+                    print("What a great tour! Congratulations!")
+                    break
+                else:
+                    print("No more possible moves!")
+                    print(f"Your knight visited {squares} squares!")
+                    break
+            else:
+                squares += 1
+            x, y = ask_move()
+else: #automated solution
+    solve()
